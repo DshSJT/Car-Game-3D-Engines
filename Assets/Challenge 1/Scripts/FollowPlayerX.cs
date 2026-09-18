36d44975ac154bcb1adb3ad87cef02dac27784b0
Daffa Shiddiq <mendezdez12@gmail.com>
2026-09-18 14:48:23 +0700

    Change camera plane

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3 (0, 4, -9);
+    private Vector3 offset = new Vector3 (40, 6, -0.7f);
 
     void Start()
     {
